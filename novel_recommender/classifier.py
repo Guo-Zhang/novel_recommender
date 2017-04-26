6e6cb698d50a60d8e0d84549b80fe88fa8dc69c6
Guo ZHANG <iguo19961110@foxmail.com>
2017-04-26 09:03:50 +0800

    update

--- novel_recommender/classifier.py
+++ novel_recommender/classifier.py
@@ -9,9 +9,11 @@ import os
 import time
 import sys
 import json
+import random
 from functools import partial
 
 import jieba
+import nltk
 from nltk import FreqDist
 from pymongo.errors import DuplicateKeyError
 
@@ -57,7 +59,7 @@ def count_words(doc):
         with codecs.open(path,'r',encoding='GB18030') as f:
             text = f.read()
     except UnicodeDecodeError as e:
-        print(e)
+        print('%s: %s'%(id, e))
         return None
 
     print("Counting %s...\r"%(id), end='')
@@ -91,6 +93,38 @@ def tf_idf():
     pass
 
 
+# -- classification --
+
+def gender_types(col):
+    docs = col.find({'_count':1},['_id'])
+    for doc in docs:
+        type = random.choice(['train','devtest','test'])
+        col.update({'_id':doc['_id']},{'$set':{'_type':type}})
+
+
+def gender_features(col, type):
+    docs = col.find({'_count':1,'_type':type},['分类'])
+    for doc in docs:
+        id = doc['_id']
+        cat = doc['分类']
+        fname = id+'.json'
+        path = os.path.join(FEATURE_PATH, fname)
+        with codecs.open(path, 'r', encoding='utf-8') as f:
+            text = f.read()
+        feature = json.loads(text)
+        yield (feature, cat)
+
+
+def classify(col):
+    gender_types(col)
+    train_set = gender_features(col,type='train')
+    devtest_set = gender_features(col,type='devtest')
+    test_set = gender_features(col,type='test')
+    print('Gendering feature data...')
+    classifier = nltk.NaiveBayesClassifier.train(train_set)
+    print(nltk.classify.accuracy(classifier, devtest_set))
+
+
 # -- main --
 
 @runtime
@@ -98,14 +132,14 @@ def main():
     collections = set_mongo()
     info = collections['novel_info']
 
-    docs = read_uncounted(info)
+    # words counting
+    # docs = read_uncounted(info)
+    # pro_con(count_words, save_count, docs)
 
-    pro_con(count_words, save_count, docs)
+    # TF-IDF
 
-    # for doc in docs:
-    #     fdist = count_words(doc)
-    #     save_count(fdist)
-    #     break
+    # classification
+    classify(info)
 
 
 if __name__=="__main__":
